55d244b1e6d35d435ff052995432a4b1c13d7e68
Makary <pereymak2012galina@yandex.ru>
2022-02-24 13:08:06 +0300

    fix linter and maxfloat

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-let getRandomInteger = function (min, max) {
+const getRandomInteger = function (min, max) {
 
   if ((min || max) < 0 || max <= min || (min || max) % 1 !== 0)  {
-    return console.log("При выборе диапозона допущена ошибка.")
+    return 'При выборе диапозона допущена ошибка.';
   }
 
   return Math.floor(Math.random() * (max - min + 1) ) + min;
-}
+};
 
 getRandomInteger(1, 50);
 
-let getRandomFloat = function (min, max, numberAfterDot) {
+const getRandomFloat = function (min, max, numberAfterDot) {
 
   if ((min || max) < 0 || max <= min || (min || max) % 1 === 0)  {
-    return console.log("При выборе диапозона допущена ошибка.")
+    return 'При выборе диапозона допущена ошибка.';
   }
 
-  let digitsDegree  = 10 ** numberAfterDot
+  const digitsDegree  = 10 ** numberAfterDot;
 
-  return Math.floor((Math.random() * (max - min) + min) * digitsDegree) / digitsDegree;
-}
+  return (Math.floor((Math.random() * (max - min) + min) * digitsDegree) + Math.round(Math.random())) / digitsDegree;
+};
 
-getRandomFloat(3.55, 6.77, 6);
+getRandomFloat(6.76, 6.77, 2);
